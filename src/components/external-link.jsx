35eedc0d5318b6f06ff81e6e6a2c40c17fb03727
Kevin Ghadyani <Saturn2888@gmail.com>
2017-09-14 02:09:12 -0500

    Updated to the latest PropTypes package

--- src/components/external-link.jsx
+++ src/components/external-link.jsx
@@ -1,48 +1,36 @@
-import React, { PureComponent, PropTypes } from 'react'
+import PropTypes from 'prop-types'
+import React from 'react'
 
-export default class ExternalLink extends PureComponent {
-	static propTypes = {
-		className: PropTypes.string,
-		children: PropTypes.element,
-		href: PropTypes.string.isRequired,
-		title: PropTypes.string,
-		name: PropTypes.string,
-		icon: PropTypes.string,
-		bold: PropTypes.bool,
-		noUnderline: PropTypes.bool,
-	};
+const ExternalLink = ({
+	bold = false,
+	children,
+	className = '',
+	href,
+	name = '',
+	noUnderline = true,
+	title = '',
+}) => (
+	<a
+		className={'no-text-underline' + (className ? ' ' + className : '')}
+		href={href}
+		title={title || name || (typeof children == 'string' && children) || null}
+		target="_blank"
+	>
+		<span className={!noUnderline ? 'text-underline' : null}>{children || name}</span>
+		&nbsp;
+		<i className={'fa ' + (bold ? 'fa-external-link-square' : 'fa-external-link')}></i>
+	</a>
+)
 
-	static defaultProps = {
-		className: '',
-		title: '',
-		name: '',
-		icon: '',
-		bold: false,
-		noUnderline: true,
-	}
-
-	render() {
-		const {
-			bold,
-			children,
-			className,
-			href,
-			name,
-			noUnderline,
-			title,
-		} = this.props
-
-		return (
-			<a
-				className={'no-text-underline' + (className ? ' ' + className : '')}
-				href={href}
-				title={title || name || (typeof children == 'string' && children) || null}
-				target="_blank"
-			>
-				<span className={!noUnderline ? 'text-underline' : null}>{children || name}</span>
-				&nbsp;
-				<i className={'fa ' + (bold ? 'fa-external-link-square' : 'fa-external-link')}></i>
-			</a>
-		)
-	}
+ExternalLink.propTypes = {
+	bold: PropTypes.bool,
+	children: PropTypes.element,
+	className: PropTypes.string,
+	href: PropTypes.string.isRequired,
+	icon: PropTypes.string,
+	name: PropTypes.string,
+	noUnderline: PropTypes.bool,
+	title: PropTypes.string,
 }
+
+export default ExternalLink
--- src/ducks/modal.jsx
+++ src/ducks/modal.jsx
@@ -2,26 +2,26 @@
 // Actions
 // --------------------------------------------------------
 
-const CHANGE_META_DATA = 'PAGE_META::CHANGE_META_DATA'
+// const CHANGE_META_DATA = 'PAGE_META::CHANGE_META_DATA'
 
 
 // --------------------------------------------------------
 // Action Creators
 // --------------------------------------------------------
 
-export const changeLocation = payload => {
-	return {
-		type: LOCATION_CHANGED,
-		payload,
-	}
-}
+// export const changeLocation = payload => {
+// 	return {
+// 		type: LOCATION_CHANGED,
+// 		payload,
+// 	}
+// }
 
-export const updatePageMeta = path => {
-	return {
-		type: CHANGE_META_DATA,
-		path
-	}
-}
+// export const updatePageMeta = path => {
+// 	return {
+// 		type: CHANGE_META_DATA,
+// 		path
+// 	}
+// }
 
 
 // --------------------------------------------------------
@@ -35,12 +35,5 @@ export const getInitialState = () => ({
 export default (state = getInitialState(), action) => {
 	const { type } = action
 
-	switch (type) {
-	// case CHANGE_META_DATA: {
-
-	// }
-
-	default:
-		return state
-	}
+	return state
 }
